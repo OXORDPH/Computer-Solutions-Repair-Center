83a6567074b38e8a5fe9eac754f8757bbd8823da
OXORDPH <ocs@oxordph.com.ph>
2025-03-20 02:09:07 +0000

    Updated custom logo module with new manifest and views

--- custom_addons/custom_logo/__manifest__.py
+++ custom_addons/custom_logo/__manifest__.py
@@ -3,15 +3,19 @@
     'version': '1.0',
     'category': 'Customization',
     'summary': 'Custom Logo and Favicon for Odoo',
+    'description': 'This module replaces the default Odoo logo and favicon with custom images.',
     'author': 'OXORDPH',
+    'website': 'https://github.com/OXORDPH/Computer-Solutions-Repair-Center',
+    'license': 'LGPL-3',
     'depends': ['web'],
     'data': [],
     'assets': {
         'web.assets_backend': [
-            'custom_logo/static/src/img/oxord-icon-512x512.png',
-            'custom_logo/static/src/img/favicon.ico',
+            '/custom_logo/static/src/img/oxord-icon-512x512.png',
+            '/custom_logo/static/src/img/favicon.ico',
         ],
     },
     'installable': True,
     'auto_install': False,
+    'application': False,
 }
