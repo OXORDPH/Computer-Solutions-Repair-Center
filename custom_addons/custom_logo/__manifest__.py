df69ae7c47d04a1d94083003e45efcac180c023f
OXORDPH <ocs@oxordph.com.ph>
2025-03-28 04:52:17 +0000

    Updated favicon and assets.xml for Odoo.sh5

--- custom_addons/custom_logo/__manifest__.py
+++ custom_addons/custom_logo/__manifest__.py
@@ -13,8 +13,8 @@
 ],
     'assets': {
         'web.assets_backend': [
-            'Computer-Solutions-Repair-Center/custom_addons/custom_logo/static/src/img/oxord-icon-512x512.png',
-            'Computer-Solutions-Repair-Center/custom_addons/custom_logo/static/src/img/favicon.ico',
+            '/custom_logo/static/src/img/oxord-icon-512x512.png',
+            '/custom_logo/static/src/img/favicon.ico',
         ],
     },
     'installable': True,
